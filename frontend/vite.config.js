82583e3cc59107f4b8eb425e3b2bc841cbeafad8
Andrea <andrea@example.com>
2026-03-26 08:59:02 +0100

    Add Vite proxy config for local dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://presenzered.crickethouse.mywire.org',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
