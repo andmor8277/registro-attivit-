b90af6ee3ef3d472ef188d51db1830095917820f
Andrea <andrea@example.com>
2026-04-03 10:45:18 +0200

    Fix PDF - revert to drawing at original size then scaling image

--- frontend/src/views/Allenamenti.vue
+++ frontend/src/views/Allenamenti.vue
@@ -661,8 +661,6 @@ function exportPdf() {
         const fieldX = 15
         const fieldHeight = fieldWidth * (canvasHeight / canvasWidth)
         const scaleRatio = fieldWidth / canvasWidth
-        const scaledCanvasW = fieldWidth
-        const scaledCanvasH = fieldHeight
         
         if (y + fieldHeight > pageHeight - 20) {
           doc.addPage()
@@ -671,14 +669,14 @@ function exportPdf() {
         
         try {
           const tempCanvas = document.createElement('canvas')
-          tempCanvas.width = scaledCanvasW
-          tempCanvas.height = scaledCanvasH
+          tempCanvas.width = canvasWidth
+          tempCanvas.height = canvasHeight
           const ctx = tempCanvas.getContext('2d')
           
-          const marginX = scaledCanvasW * 0.03
-          const marginY = scaledCanvasH * 0.03
-          const fieldW = scaledCanvasW - marginX * 2
-          const fieldH = scaledCanvasH - marginY * 2
+          const marginX = canvasWidth * 0.03
+          const marginY = canvasHeight * 0.03
+          const fieldW = canvasWidth - marginX * 2
+          const fieldH = canvasHeight - marginY * 2
           
           const stripeCount = 12
           const stripeWidth = fieldW / stripeCount
@@ -694,16 +692,16 @@ function exportPdf() {
             ctx.strokeRect(marginX, marginY, fieldW, fieldH)
             
             ctx.beginPath()
-            ctx.moveTo(scaledCanvasW / 2, marginY)
-            ctx.lineTo(scaledCanvasW / 2, scaledCanvasH - marginY)
+            ctx.moveTo(canvasWidth / 2, marginY)
+            ctx.lineTo(canvasWidth / 2, canvasHeight - marginY)
             ctx.stroke()
             
             const centerR = fieldW * 0.15
             ctx.beginPath()
-            ctx.arc(scaledCanvasW / 2, scaledCanvasH / 2, centerR, 0, Math.PI * 2)
+            ctx.arc(canvasWidth / 2, canvasHeight / 2, centerR, 0, Math.PI * 2)
             ctx.stroke()
             ctx.beginPath()
-            ctx.arc(scaledCanvasW / 2, scaledCanvasH / 2, 3, 0, Math.PI * 2)
+            ctx.arc(canvasWidth / 2, canvasHeight / 2, 3, 0, Math.PI * 2)
             ctx.fillStyle = '#fff'
             ctx.fill()
             
@@ -715,44 +713,44 @@ function exportPdf() {
             const goalHeight = fieldH * 0.07
             const goalDepth = fieldW * 0.02
             
-            const smallTop = scaledCanvasH / 2 - smallHeight / 2
-            const penaltyTop = scaledCanvasH / 2 - penaltyHeight / 2
+            const smallTop = canvasHeight / 2 - smallHeight / 2
+            const penaltyTop = canvasHeight / 2 - penaltyHeight / 2
             
             ctx.strokeRect(marginX, smallTop, smallDepth, smallHeight)
-            ctx.strokeRect(scaledCanvasW - marginX - smallDepth, smallTop, smallDepth, smallHeight)
+            ctx.strokeRect(canvasWidth - marginX - smallDepth, smallTop, smallDepth, smallHeight)
             
             ctx.strokeRect(marginX, penaltyTop, penaltyDepth, penaltyHeight)
-            ctx.strokeRect(scaledCanvasW - marginX - penaltyDepth, penaltyTop, penaltyDepth, penaltyHeight)
+            ctx.strokeRect(canvasWidth - marginX - penaltyDepth, penaltyTop, penaltyDepth, penaltyHeight)
             
             const lunetteCenterX = marginX + penaltyDepth
-            const lunetteCenterXRight = scaledCanvasW - marginX - penaltyDepth
+            const lunetteCenterXRight = canvasWidth - marginX - penaltyDepth
             
             ctx.beginPath()
-            ctx.arc(lunetteCenterX, scaledCanvasH / 2, arcR, -Math.PI / 2, Math.PI / 2)
+            ctx.arc(lunetteCenterX, canvasHeight / 2, arcR, -Math.PI / 2, Math.PI / 2)
             ctx.strokeStyle = '#fff'
             ctx.stroke()
             ctx.beginPath()
-            ctx.arc(lunetteCenterXRight, scaledCanvasH / 2, arcR, Math.PI / 2, -Math.PI / 2)
+            ctx.arc(lunetteCenterXRight, canvasHeight / 2, arcR, Math.PI / 2, -Math.PI / 2)
             ctx.stroke()
             
             const penaltySpotX = fieldW * 0.105
             
             ctx.beginPath()
-            ctx.arc(marginX + penaltySpotX, scaledCanvasH / 2, 3, 0, Math.PI * 2)
+            ctx.arc(marginX + penaltySpotX, canvasHeight / 2, 3, 0, Math.PI * 2)
             ctx.fillStyle = '#fff'
             ctx.fill()
             ctx.beginPath()
-            ctx.arc(scaledCanvasW - marginX - penaltySpotX, scaledCanvasH / 2, 3, 0, Math.PI * 2)
+            ctx.arc(canvasWidth - marginX - penaltySpotX, canvasHeight / 2, 3, 0, Math.PI * 2)
             ctx.fill()
             
-            ctx.strokeRect(marginX - goalDepth, scaledCanvasH / 2 - goalHeight / 2, goalDepth, goalHeight)
-            ctx.strokeRect(scaledCanvasW - marginX, scaledCanvasH / 2 - goalHeight / 2, goalDepth, goalHeight)
+            ctx.strokeRect(marginX - goalDepth, canvasHeight / 2 - goalHeight / 2, goalDepth, goalHeight)
+            ctx.strokeRect(canvasWidth - marginX, canvasHeight / 2 - goalHeight / 2, goalDepth, goalHeight)
           }
           
           const elementi = ex.elementi || []
           for (const el of elementi) {
-            const x = (el.x / 100) * scaledCanvasW
-            const yPos = (el.y / 100) * scaledCanvasH
+            const x = (el.x / 100) * canvasWidth
+            const yPos = (el.y / 100) * canvasHeight
             const rot = (el.rotazione || 0) * Math.PI / 180
             
             ctx.save()
